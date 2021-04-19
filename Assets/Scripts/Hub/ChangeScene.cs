6def1e3aee82b2c561239fcc546264efee5a3f90
CalebBaudoin <c00304031@louisiana.edu>
2021-04-19 15:12:12 -0500

    Got a bouncy ball, made the change scene script more correct

--- Assets/Scripts/Hub/ChangeScene.cs
+++ Assets/Scripts/Hub/ChangeScene.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    int rng = 0;
 
     public GameObject thisObject;
     // Start is called before the first frame update
@@ -16,18 +17,32 @@ public class ChangeScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rng = Random.Range(0, 999) % 3;
+        Debug.Log(rng);
     }
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Hand") && thisObject.CompareTag("Arcade")){
-            Debug.Log("Fack");
+            
             SceneManager.LoadScene("ArcadeTest");
             
         }
         if (other.CompareTag("Hand") && thisObject.CompareTag("Labyrinth")){
-            Debug.Log("Fack");
+
+            rng = Random.Range(0, 2);
             SceneManager.LoadScene("Labyrinths");
+            /*
+             if(rng == 0){
+                SceneManager.LoadScene("FirstLabyrinth"); 
+             }
+             else if(rng == 1){
+                SceneManager.LoadScene("RoomLab");
+             }
+             else if (rng == 2){
+                SceneManager.LoadScene("BasicMaze");
+             }
+             */
 
         }
     }
