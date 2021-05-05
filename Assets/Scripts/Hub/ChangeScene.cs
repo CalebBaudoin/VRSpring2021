e8c5c126f89a7f5adb2b8a03d01bbb8b6ec16ed7
CalebBaudoin <57913168+CalebBaudoin@users.noreply.github.com>
2021-05-04 22:52:00 -0500

    Can now switch scenes

--- Assets/Scripts/Hub/ChangeScene.cs
+++ Assets/Scripts/Hub/ChangeScene.cs
@@ -6,7 +6,7 @@ using Valve.VR;
 
 public class ChangeScene : MonoBehaviour
 {
-    int rng = 0;
+
 
     public GameObject thisObject;
     // Start is called before the first frame update
@@ -18,8 +18,7 @@ public class ChangeScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rng = Random.Range(0, 999) % 3;
-        //Debug.Log(rng);
+     
     }
 
     void test()
@@ -29,33 +28,21 @@ public class ChangeScene : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
       
-        if (other.CompareTag("Hands") && thisObject.CompareTag("Arcade")){
-            
-            //SceneManager.LoadScene("ArcadeTest");
-            SteamVR_LoadLevel.Begin("ArcadeTest");
-            
+        if (other.CompareTag("Hands") && thisObject.CompareTag("Rooms")){
+           
+            SceneManager.LoadScene("RoomLab");
+            //SteamVR_LoadLevel.Begin("RoomLab");
+
         }
         if (other.CompareTag("Hands") && thisObject.CompareTag("Labyrinth")){
 
-            rng = Random.Range(0, 2);
-            SceneManager.LoadScene("Labyrinths");
-            //SteamVR_LoadLevel.Begin("Labyrinths");
-            //SceneManager.LoadScene("RoomLab");
-            //SteamVR_LoadLevel.Begin("RoomLab");
-             /*if(rng == 0){
-                //SteamVR_LoadLevel.Begin("FirstLabyrinth");
-                SceneManager.LoadScene("FirstLabyrinth"); 
-             }
-             else if(rng == 1){
-                //SceneManager.LoadScene("RoomLab");
-                SteamVR_LoadLevel.Begin("RoomLab");
-             }
-             else if (rng == 2){
-                //SteamVR_LoadLevel.Begin("BasicMaze");
-                SceneManager.LoadScene("BasicMaze");
-             }*/
-             
+            SceneManager.LoadScene("FirstLabyrinth");
+            //SteamVR_LoadLevel.Begin("FirstLabyrinth");
 
+        }
+        if (other.CompareTag("Hands") && thisObject.CompareTag("Maze")){
+            //SteamVR_LoadLevel.Begin("BasicMaze");
+            SceneManager.LoadScene("BasicMaze");
         }
     }
 
